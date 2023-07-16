1ccfb58c1c2132841c27ac6ff62bd1a17e5fec68
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-16 21:41:42 +0200

    write selenium login test

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,29 @@
-from selenium import webdriver
+from selenium import webdriver;
+from selenium.webdriver.common.by import By;
 
 driver = webdriver.Chrome()
 
+# navigate to login page
 driver.get("https://betsy-webshop-remake.ew.r.appspot.com/login")
 
+# print login page title
 print(driver.title)
+
+# clear field from previous data
+driver.find_element(By.NAME, "username").clear()
+
+# enter text
+driver.find_element(By.NAME, "username").send_keys("Marietje")
+
+# click submit button
+driver.find_element(By.CSS_SELECTOR, "input[type=submit]").click()
+
+# get page title - should be profile page
+print(driver.title)
+
+# get welcome text on the profile page
+message = driver.find_element(By.CSS_SELECTOR, "h2").text
+
+# print the welcome message 
+print(message)
+
